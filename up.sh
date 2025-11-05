3fc70bb067e1a64a6e196a72c8d24f1dde9de5f8
lhackerfou <root@git-drieux.local>
2025-11-05 08:37:54 +0000

    j ai faim

--- up.sh
+++ up.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 git add index.html
-git commit -m "j ai faim"
 git add script.sh
-git commit -m "j ai faim"
 git add README.md
-git commit -m "j ai faim"
 git add sl.sh
-git commit -m "j ai faim"
 git add up.sh
 git commit -m "j ai faim"
 git push -u origin main
